593a5eb0a767bb6bedc8fa22dfdda520c955afd1
Rebecca <rebeccamancini.student@careerdevs.com>
2018-11-13 23:01:05 +0000

    Routing updates

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
+// Load user model
+const User = require('../../models/User');
+
 router.get('/test', (req, res) => res.json({msg: 'Users Works'}));
+
+router.post('/register', (req, res) => User.findOne({ email: req.body.email }) 
+});
 
 module.exports = router;
