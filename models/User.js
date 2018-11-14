69ef8f38d598f842dadb12bf73fd413692d54ac3
Rebecca <rebeccamancini.student@careerdevs.com>
2018-11-14 02:09:22 +0000

    Password and response

---
+++ models/User.js
@@ -0,0 +1,22 @@
+// Create Schema
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+email: {
+    type: String,
+    required: true
+},
+password: {
+    type: String,
+    required: true
+},
+avatar: {
+    type: String,
+},
+date: {
+    type: Date,
+    default: Date.now
+}
+})
