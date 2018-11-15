d65a25ec28b82ff7fe0b2919c39942233a779adb
Rebecca <rebeccamancini.student@careerdevs.com>
2018-11-15 04:13:29 +0000

    Validator initialized

--- routes/api/users.js
+++ routes/api/users.js
@@ -4,6 +4,11 @@ const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const keys = require('../../config/keys');
+const passport = require('passport');
+
+// Load Input Validation
+const validateRegisterInput = require('../../validation/register');
+
 // Load user model
 const User = require('../../models/User');
 
@@ -15,11 +20,18 @@ router.get('/test', (req, res) => res.json({msg: 'Users Works'}));
 // @route  GET api/users/test
 // @desc  Tests users route
 // @access  Public
-router.post('/register', (req, res) => User.findOne({ email: req.body.email }) 
-
-.then(user => {
+router.post('/register', (req, res) => {
+const { errors, isValid } = validateRegisterInput(req.body);
+ 
+ // Check Validation
+ if (!isValid) {
+     return res.status(400).json(errors);
+ }
+ 
+User.findOne({ email: req.body.email }).then(user => {
     if(user) {
-        return res.status(400).json({email: 'Email already exists'});
+        errors.email = 'Email already exists';
+        return res.status(400).json(errors);
     } else {
         const avatar = gravatar.url(req.body.email, {
             s: '200', // Size
